adc41194d9731ea9970781e9ae1a4959391ef6af
조법룡 <jby0711823@naver.com>
2022-01-13 02:03:23 +0900

    content : img 최대 사이즈 수정 및 회고 일부 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
+              maxWidth: 500,
               linkImagesToOriginal: false,
             },
           },
